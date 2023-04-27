27eb45f47aa4eda25900b22fbdd1e213cc67f1e0
Henrique Ribeiro Rocha Neves <henrique.neves@creaes.corp>
2023-04-27 15:09:06 -0300

    Trabalho UVV.

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
 
 export default function App() {
+
+  let [filmes, setFilmes] = useState([]);
+
+  const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
+
+  useEffect(function () {
+    fetch(baseURL)
+      .then(data => data.json())
+      .then(objeto => {
+        console.log(objeto);
+        console.log(objeto.data);
+        setFilmes(objeto.data);
+      })
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      { filmes.length > 0 ? filmes.map(filme =>
+        <Text>
+          {'\n\n'}TITULO: {filme.attributes.titulo}
+          <Text>
+            {'\n\n'}SINOPSE: {filme.attributes.sinopse}
+          </Text>
+        </Text>) : <Text>Carregando...</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent:'center'
   },
 });
